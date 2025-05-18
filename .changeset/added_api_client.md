b0b4ddd5f311321a699266746b5b1af6e37c209b
Nate <me@n8m.us>
2025-05-18 12:02:15 -0700

    add api client

---
+++ .changeset/added_api_client.md
@@ -0,0 +1,5 @@
+---
+default: patch
+---
+
+# Added API client
--- api/client.go
+++ api/client.go
@@ -1,1 +1,28 @@
 package api
+
+import (
+	"context"
+
+	"go.sia.tech/jape"
+	"go.sia.tech/troubleshootd/troubleshoot"
+)
+
+// Client is a client for the troubleshoot API.
+type Client struct {
+	c jape.Client
+}
+
+// TestConnection tests the host's connection to the API server.
+func (c *Client) TestConnection(ctx context.Context, host troubleshoot.Host) (result troubleshoot.Result, err error) {
+	err = c.c.POST(ctx, "/troubleshoot", host, &result)
+	return
+}
+
+// NewClient creates a new client for the troubleshoot API.
+func NewClient(addr string) *Client {
+	return &Client{
+		c: jape.Client{
+			BaseURL: addr,
+		},
+	}
+}
